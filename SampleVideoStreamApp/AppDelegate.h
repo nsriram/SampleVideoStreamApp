eb939d2c18401e5d325d3c0816de7aed2e593e34
Sriram Iyengar <narasis@thoughtworks.com>
2011-12-18 12:13:10 +0530

    Initial Commit

---
+++ SampleVideoStreamApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SampleVideoStreamApp
+//
+//  Created by Sriram Narasimhan on 18/12/11.
+//  Copyright (c) 2011 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SampleVideoStreamApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleVideoStreamApp
+//
+//  Created by Sriram Narasimhan on 18/12/11.
+//  Copyright (c) 2011 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
